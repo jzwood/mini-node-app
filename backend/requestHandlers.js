27cfe3a363a382390cd562f8ce33b9aa9f2c9f94
Jake Wood <jake.wood@sfm1pcg8wml.na.akqa.local>
2016-04-19 22:01:12 -0700

    clean input query parsing and pw encryption. time to add sqlite

--- backend/requestHandlers.js
+++ backend/requestHandlers.js
@@ -7,27 +7,32 @@ var querystring = require("querystring"),
     loader = require("./loadFile"),
     sha1 = require('sha1');//for 1 way hash encryptions // let ct = sha1("message");
 
-// function home(response, postData){
-//
-// }
+function home(response, postData){
+
+}
 
 function start(response, postData) {
 
-  var context = {title: "My New Post", posted: postData};
+  var login = querystring.parse(postData).login,
+  pw = querystring.parse(postData).pw,
+  secure_pw = sha1(String(pw));
+  title = ["Hi. Who are you?","Nope, can't find you."],
+  heading = title[0];
+
+  var context = {"heading": heading, "login": login, "pw":secure_pw};
   loader.loadHTML(response,'/templates/body.hbs',context);
 
 }
 
 function upload(response, postData){
-  // console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: "+ querystring.parse(postData).text);
-  response.end();
-  // start(response,querystring.parse(postData).text);
-
+  //console.log("Request handler 'upload' was called.");
+  // response.writeHead(200, {"Content-Type": "text/plain"});
+  // response.write("You've sent the text: "+ postData);//querystring.parse(postData).text);
+  // response.end();
+  start(response,postData);
 }
 
-exports.home = start;
+exports.home = home;
 exports.start = start;
 exports.upload = upload;
 exports.loadResource = loader.loadResource;
